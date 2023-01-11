d0bc5bbd24dcdab4d66a5374376b2dcf26861e95
Gaston Pioli <milton.pioli@despegar.com>
2023-01-10 22:53:08 -0300

    Updates

--- _001-HelloWorld/src/HelloWorld2.java
+++ _001-HelloWorld/src/HelloWorld2.java
@@ -1,5 +1,24 @@
 public class HelloWorld2 {
     public static void main(String[] args) {
-        System.out.println("Hello, my name is Gastón");
+        String salute = "Hello, my name is Gastón";
+        System.out.println(salute);
+        // concatenating text
+        System.out.println("salute.toUpperCase() = " + salute.toUpperCase());
+
+        // printing an int (primitive)
+        int num = 10;
+        System.out.println("num = " + num);
+
+        // working with the boolean data type
+        boolean value = true;
+
+        if(value){
+            System.out.println("It's true!");
+        }
+
+        // From JDK 10 we have available dynamic var assignation
+        var numero3 = "15";
+        System.out.println((numero3).getClass());
+
     }
 }
