3309b3de1fd7f08a20450c5fec2f47a98219dc99
Gaston Pioli <milton.pioli@despegar.com>
2023-01-14 17:49:09 -0300

    working with data type conversion

--- _001-HelloWorld/src/X007_TypeConversion.java
+++ _001-HelloWorld/src/X007_TypeConversion.java
@@ -16,6 +16,36 @@ public class X007_TypeConversion {
         String boolStr = "TRue";
         boolean logicBool = Boolean.parseBoolean(boolStr);
         System.out.println("logicBool = " + logicBool); // notive it ignores case
+
+
+        int otherIntNumber = 100;
+        System.out.println("otherIntNumber = " + otherIntNumber);
+
+        String otherStrNumber = Integer.toString(otherIntNumber);
+
+        otherStrNumber = String.valueOf(otherStrNumber);
+        System.out.println("otherStrNumber = " + otherStrNumber);
+
+        double otherRealDouble = 1.23456;
+        String otherRealStr = Double.toString(otherRealDouble);
+        System.out.println("otherRealStr = " + otherRealStr);
         
+        otherRealStr = String.valueOf(1.23456e2f);
+        System.out.println("otherRealStr = " + otherRealStr);
+
+
+        // Primitives conversion:
+
+        int i = 32767;
+        short s = (short)i; // here we will have data loss, as the value to cast is greater than the destination type
+        System.out.println("s = " + s);
+        long l = i;
+        System.out.println("l = " + l);
+        System.out.println(Short.MAX_VALUE);
+        char b = (char) i;
+        System.out.println("b = " + b);
+        float f = (float) i;
+        System.out.println("f = " + f);
+
     }
 }
