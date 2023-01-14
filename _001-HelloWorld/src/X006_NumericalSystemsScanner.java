822b0d3b4a1ee8aba12fa01623d0eb72d1d0d8f4
Gaston Pioli <milton.pioli@despegar.com>
2023-01-14 16:57:15 -0300

    Working with Scanner and System.in

--- _001-HelloWorld/src/X006_NumericalSystemsScanner.java
+++ _001-HelloWorld/src/X006_NumericalSystemsScanner.java
@@ -1,39 +1,28 @@
 import javax.swing.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class X006_NumericalSystemsScanner {
     public static void main(String[] args) {
 
-        String strNumber = JOptionPane.showInputDialog(null, "Please, enter an Integer number");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, enter an Integer number: ");
 
         int decNumber = 0;
         try {
-            decNumber = Integer.parseInt(strNumber);
-        } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(null, "Error: only Integer numers are accepted");
+            decNumber = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Error: only integer numers are accepted");
             main(args); // if the error happens, the idea is to repeat the process
             System.exit(0);
         }
 
-        System.out.println("decNumber = " + decNumber);
+        System.out.println("Decimal number = " + decNumber);
 
-        String binaryMessage = "binary number of " + decNumber + " = " + Integer.toBinaryString(decNumber);
-        System.out.println(binaryMessage);
-        
-        int binaryNumber = 0b111110100; // 0b indicates java that the number is binary
-        // when we print it java will print it as dec
-        System.out.println("binaryNumber = " + binaryNumber);
-
-        String octalMessage = "octal number of " + decNumber + " = " + Integer.toOctalString(decNumber);
-        System.out.println(octalMessage);
-        int octalNumber = 0764; // the 0 indicates java its in Octal system
-
-        String hexadecimalMessage = "hexadecimal number of " + decNumber + " = " + Integer.toHexString(decNumber);
-        System.out.println(hexadecimalMessage);
-        int hexNumber = 0x1f4; // the 0 indicates java its in Octal system
 
         String message = "Binary number of " + decNumber + " = " + Integer.toBinaryString(decNumber);
-        message += "\n octal number of " + decNumber + " = " + Integer.toOctalString(decNumber);
-        message += "\n hexadecimal number of " + decNumber + " = " + Integer.toHexString(decNumber);
-        JOptionPane.showMessageDialog(null, message);
+        message += "\nOctal number of " + decNumber + " = " + Integer.toOctalString(decNumber);
+        message += "\nHexadecimal number of " + decNumber + " = " + Integer.toHexString(decNumber);
+        System.out.println(message);
     }
 }
