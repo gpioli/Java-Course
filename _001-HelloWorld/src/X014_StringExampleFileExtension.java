6848f63b434a9e5cf5d8c6bcc1917184eef85d67
Gaston Pioli <milton.pioli@despegar.com>
2023-02-04 21:12:01 -0300

    File extension getter class

---
+++ _001-HelloWorld/src/X014_StringExampleFileExtension.java
@@ -0,0 +1,14 @@
+public class X014_StringExampleFileExtension {
+    public static void main(String[] args) {
+
+        // function for obtaining file extension:
+
+        String filename = "some_image.jpeg";
+        System.out.println("filename.length() = " + filename.length());
+        System.out.println("filename.substring(filename.length()-4) = " + filename.substring(filename.length()-4));
+
+        int i = filename.lastIndexOf(".");
+        System.out.println("filename extension = " + filename.substring(i+1));
+        
+    }
+}
