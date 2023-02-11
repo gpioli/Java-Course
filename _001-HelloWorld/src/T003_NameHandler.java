79a458e17b6f95334801afafb90552080b1875f3
Gaston Pioli <milton.pioli@despegar.com>
2023-02-11 18:37:58 -0300

    Task 3: name handler

--- _001-HelloWorld/src/T003_NameHandler.java
+++ _001-HelloWorld/src/T003_NameHandler.java
@@ -11,33 +11,34 @@ public class T003_NameHandler {
 //        N.es_A.ia_E.pe
 
         Scanner scanner = new Scanner(System.in);
-        String person1 = null;
-        String person2 = null;
-        String person3 = null;
 
         System.out.println("Welcome to the names handler program");
 
         try {
             System.out.println("Please, enter the name of the first person");
-            person1 = scanner.nextLine().trim();
+            String person1 = scanner.nextLine().trim();
             System.out.println("Please, enter the of the second person");
-            person2 = scanner.nextLine().trim();
+            String person2 = scanner.nextLine().trim();
             System.out.println("Please, enter the of the third person");
-            person3 = scanner.nextLine().trim();
+            String person3 = scanner.nextLine().trim();
+
+            Character name1Init = person1.toCharArray()[1];
+            String name1InitMay = name1Init.toString().toUpperCase();
+            String newName1 = name1InitMay + "." + person1.substring(person1.length()-2);
+
+            Character name2Init = person2.toCharArray()[1];
+            String name2InitMay = name2Init.toString().toUpperCase();
+            String newName2 = name2InitMay + "." + person2.substring(person2.length()-2);
+
+            Character name3Init = person3.toCharArray()[1];
+            String name3InitMay = name3Init.toString().toUpperCase();
+            String newName3 = name3InitMay + "." + person3.substring(person3.length()-2);
+
+            System.out.println(newName1 + "_" + newName2 + "_" + newName3);
+
         } catch (Exception e) {
-            System.out.println("Only string input allowed. Error: " + e);
+            System.out.println("Only string input allowed. ");
         }
-
-        Character newName1begin = person1.toCharArray()[1];
-        String newName1may = newName1begin.toString().toUpperCase();
-
-
-        String newName1 = person1.toCharArray()[1] + person1.substring(person1.length()-2);
-        String newName2 = person2.toCharArray()[1] + person2.substring(person2.length()-2);
-        String newName3 = person3.toCharArray()[1] + person3.substring(person3.length()-2);
-
-        System.out.println(newName1 + "_" + newName2 + "_" + newName3);
-
 
     }
 }
