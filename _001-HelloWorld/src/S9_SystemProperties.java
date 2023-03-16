0ae0838968785a8fc6df7386f1c0094e40763b64
Gaston Pioli <milton.pioli@despegar.com>
2023-03-16 01:09:05 -0300

    Working with system properties

---
+++ _001-HelloWorld/src/S9_SystemProperties.java
@@ -0,0 +1,30 @@
+import java.util.Properties;
+
+public class S9_SystemProperties {
+    public static void main(String[] args) {
+
+        // More info here: https://docs.oracle.com/javase/tutorial/essential/environment/sysprop.html
+
+        String username = System.getProperty("user.name");
+        System.out.println("username = " + username);
+
+        String home = System.getProperty("user.home");
+        System.out.println("home = " + home);
+
+        String workspace = System.getProperty("user.dir");
+        System.out.println("workspace = " + workspace);
+
+        String javaVersion = System.getProperty("java.version");
+        System.out.println("javaVersion = " + javaVersion);
+
+        String lineSeparator = System.getProperty("line.separator");
+        System.out.println("lineSeparator = " + lineSeparator + " this will print in a new line");
+        String lineSeparator2 = System.lineSeparator();
+        System.out.println("lineSeparator2 = " + lineSeparator2 + "this will print in a new line");
+
+        // Let's print all yhe system properties
+        Properties p = System.getProperties();
+        p.list(System.out);
+
+    }
+}
