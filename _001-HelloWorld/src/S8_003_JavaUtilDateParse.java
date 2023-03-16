95b5c1870e5da5cfaf321fbe6ff0dddbe2fc4adf
Gaston Pioli <milton.pioli@despegar.com>
2023-03-15 23:52:24 -0300

    Parsing dates from text and comparing dates

--- _001-HelloWorld/src/S8_003_JavaUtilDateParse.java
+++ _001-HelloWorld/src/S8_003_JavaUtilDateParse.java
@@ -19,6 +19,30 @@ public class S8_003_JavaUtilDateParse {
             Date date = format.parse(s.next());
             System.out.println("date without format = " + date);
             System.out.println("date using described format = " + format.format(date));
+
+
+            // Comparing dates
+            Date date2 = new Date();
+
+            if (date.after(date2)){
+                System.out.println("Date 1 (inputted by user) is after date2 (actual date)");
+            } else if (date.before(date2)) {
+                System.out.println("Date 1 (inputted by user) is before date2 (actual date)");
+            } else if (date.equals(date2)) {
+                System.out.println("Date 1 (inputted by user) is equal to date2 (actual date)");
+            }
+
+            System.out.println("Comparison of dates 2");
+
+            // Another way of comparing dates
+            if (date.compareTo(date2) > 0){
+                System.out.println("Date 1 (inputted by user) is after date2 (actual date)");
+            } else if (date.compareTo(date2) < 0) {
+                System.out.println("Date 1 (inputted by user) is before date2 (actual date)");
+            } else if (date.compareTo(date2) == 0) {
+                System.out.println("Date 1 (inputted by user) is equal to date2 (actual date)");
+            }
+
         } catch (ParseException e) {
             e.printStackTrace();
         }
