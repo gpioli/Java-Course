264b1be6141532bd8731df9700654d81f363f0d8
Gaston Pioli <milton.pioli@despegar.com>
2023-03-22 23:38:25 -0300

    working with args and command line

--- _001-HelloWorld/src/S11_001_ArgsCommandLine.java
+++ _001-HelloWorld/src/S11_001_ArgsCommandLine.java
@@ -1,2 +1,24 @@
 public class S11_001_ArgsCommandLine {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("You must enter at least one argument");
+            System.exit(-1);
+        }
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Argument n° " + i + " : " + args[i]);
+        }
+
+        // compile: javac <filename.java>
+        // run: java <filename.class>
+
+        // java S11_001_ArgsCommandLine Gaston Ana Laura
+
+        // Response:
+        // Argument n° 0 : Gaston
+        // Argument n° 1 : Ana
+        // Argument n° 2 : Laura
+
+    }
 }
--- _001-HelloWorld/src/S11_002_ArgsCommandLineCalculator.java
+++ _001-HelloWorld/src/S11_002_ArgsCommandLineCalculator.java
@@ -1,2 +1,37 @@
 public class S11_002_ArgsCommandLineCalculator {
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Please enter an operation (sum/substraction/multiplication/division)" +
+                    " and two integers\n");
+        }
+
+        String operation = args[0];
+        int a = Integer.parseInt(args[1]);
+        int b = Integer.parseInt(args[2]);
+        double result = 0.00;
+
+        switch (operation) {
+            case "sum":
+                result = a + b;
+                break;
+            case "substraction":
+                result = a - b;
+                break;
+            case "multiplication":
+                result = a * b;
+                break;
+            case "division":
+                if (b == 0) {
+                    System.out.println("Cero division is not allowed! ");
+                    System.exit(-1);
+                }
+                result = (double)a/b;
+                break;
+            default:
+                result = a+b;
+        }
+
+        System.out.println("Result of the operation is: " + result);
+
+    }
 }
