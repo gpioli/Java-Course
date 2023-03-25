6b19f883500c199eadcfa30b62b9727a5fa58204
Gaston Pioli <milton.pioli@despegar.com>
2023-03-25 01:15:25 -0300

    Bubble optimized algorithm

---
+++ _002-Arrays/src/S12_005_BubbleOrderingOptimized.java
@@ -0,0 +1,36 @@
+public class S12_005_BubbleOrderingOptimized {
+
+    public static void main(String[] args) {
+
+
+        String[] products = {"Kingston Pendrive 64GB", "Samsung Galaxy", "Samsung External Hard Drive", "Notebook Asus",
+                "Macbook Air", "Chromecast 4th generation", "Oxford bicycle"};
+        int total = products.length;
+
+        // Here we use the bubble algorithm optimized.
+        // We compare the actual element with the next
+
+        // CompareTo method compares String using position in the unicode table.
+        // less than 0 means it is in a previous position, and more than cero means
+        int counter = 0;
+        for (int i = 0; i < total; i++) {
+            for (int j = 0; j < total - 1 - i; j++) {
+                if (products[j + 1].compareTo(products[j]) < 0) { // if i is in a lower value in the unicode table, we exchange the products.
+                    String aux = products[i];
+                    products[i] = products[j];
+                    products[j] = aux;
+                }
+                counter++;
+            }
+        }
+        System.out.println("counter = " + counter);
+
+        System.out.println("\n=== Ordered array ===");
+        for (int i = 0; i < total; i++) {
+            System.out.println("Index " + i + " : " + products[i]);
+        }
+
+    }
+
+
+}
