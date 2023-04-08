66b1ab7e331696c40f371b9eaca299ef46bffa49
Gaston Pioli <milton.pioli@despegar.com>
2023-04-08 16:34:17 -0300

    POO

--- _004-POO-I/src/Car.java
+++ _004-POO-I/src/Car.java
@@ -3,4 +3,15 @@ public class Car {
     String model;
     String color = "grey"; // default value
     double cc;
+
+    // creating our first method:
+    public String detail(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("\ncar.manufacturer = " + this.manufacturer);
+        sb.append("\ncar.model = " + this.model);
+        sb.append("\ncar.color = " + this.color);
+        sb.append("\ncar.cc = " + this.cc);
+        return sb.toString();
+    };
+
 }
--- _004-POO-I/src/S14_001_Car.java
+++ _004-POO-I/src/S14_001_Car.java
@@ -3,24 +3,22 @@ public class S14_001_Car {
     public static void main(String[] args) {
 
         // Instantiating the object
-        Car car = new Car();
+        Car subaru = new Car();
 
         // this will print default values
-        System.out.println("car.manufacturer = " + car.manufacturer);
-        System.out.println("car.model = " + car.model);
-        System.out.println("car.color = " + car.color);
-        System.out.println("car.cc = " + car.cc);
+        System.out.println("car.manufacturer = " + subaru.manufacturer);
+        System.out.println("car.model = " + subaru.model);
+        System.out.println("car.color = " + subaru.color);
+        System.out.println("car.cc = " + subaru.cc);
 
-        car.manufacturer = "Subaru";
-        car.model = "Impreza";
-        car.cc = 2.0;
-        car.color = "White";
+        subaru.manufacturer = "Subaru";
+        subaru.model = "Impreza";
+        subaru.cc = 2.0;
+        subaru.color = "White";
 
         System.out.println();
-        System.out.println("car.manufacturer = " + car.manufacturer);
-        System.out.println("car.model = " + car.model);
-        System.out.println("car.color = " + car.color);
-        System.out.println("car.cc = " + car.cc);
+        subaru.detail();
+
 
         Car mazda = new Car();
         mazda.manufacturer = "Mazda";
@@ -29,10 +27,8 @@ public class S14_001_Car {
         mazda.color = "Red";
 
         System.out.println();
-        System.out.println("mazda.manufacturer = " + mazda.manufacturer);
-        System.out.println("car.model = " + mazda.model);
-        System.out.println("mazda.color = " + mazda.color);
-        System.out.println("mazda.cc = " + mazda.cc);
+        System.out.println(mazda.detail());
+        System.out.println(subaru.detail());
 
     }
 
