41b9986f1379623cc62c3fac86b0b8e6006d3bee
Gaston Pioli <milton.pioli@despegar.com>
2023-04-07 22:05:31 -0300

    working with identity matrix

---
+++ _003-Matrix/src/S13_010_IdentityMatrix.java
@@ -0,0 +1,22 @@
+public class S13_010_IdentityMatrix {
+    public static void main(String[] args) {
+
+        int[][] matrix = new int[5][5];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i == j) {
+                    matrix[i][j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
+
