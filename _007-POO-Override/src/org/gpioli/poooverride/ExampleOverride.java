b2cc3f8299eb29aa3fc0f0acb2c53156d1994f77
Gaston Pioli <milton.pioli@despegar.com>
2023-04-19 22:41:43 -0300

    Override example

---
+++ _007-POO-Override/src/org/gpioli/poooverride/ExampleOverride.java
@@ -0,0 +1,26 @@
+package org.gpioli.poooverride;
+
+public class ExampleOverride {
+    public static void main(String[] args) {
+
+        Calculator cal = new Calculator();
+
+        System.out.println("Sum int: " + cal.sum(10, 5));
+        System.out.println("Sum float: " + cal.sum(10.0f, 5f));
+        System.out.println("Sum float-int: " + cal.sum(10.0f, 5));
+        System.out.println("Sum int-float: " + cal.sum(10, 5.0F));
+        System.out.println("Sum double: " + cal.sum(10.0, 5.0));
+        System.out.println("Sum String: " + cal.sum("10", "5"));
+        System.out.println("Sum three int: " + cal.sum(10, 5, 3));
+
+        // What happens when we pass parameters that don't match with any method?
+        // Type automatic conversion
+        System.out.println("Sum long: " + cal.sum(10L, 5L));
+        System.out.println("Sum int: " + cal.sum(10, '@')); // Here we input a char, and what happens is that
+        // we are summing the decimal representation:
+        // https://en.wikipedia.org/wiki/List_of_Unicode_characters
+        // @ -> decimal rep: 64
+        System.out.println("Sum float-int: " + cal.sum(10F, '@'));
+
+    }
+}
