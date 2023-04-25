1d80497ec41de9c2be4ecb77b313bc0a6d84affe
Gaston Pioli <milton.pioli@despegar.com>
2023-04-25 00:21:06 -0300

    T21 -> People of a company

---
+++ _008-T21-CompanyPeople/src/org/piolicompany/com/Client.java
@@ -0,0 +1,29 @@
+package org.piolicompany.com;
+
+public class Client extends Person {
+
+    private int clientId;
+    private int clientIdIndex;
+
+
+    public Client(String firstName, String lastName) {
+        super(firstName, lastName);
+        this.clientId = ++clientIdIndex;
+    }
+
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "clientId: " + clientId;
+
+    }
+}
---
+++ _008-T21-CompanyPeople/src/org/piolicompany/com/Person.java
@@ -0,0 +1,53 @@
+package org.piolicompany.com;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private String fiscalNumber;
+    private String address;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFiscalNumber() {
+        return fiscalNumber;
+    }
+
+    public void setFiscalNumber(String fiscalNumber) {
+        this.fiscalNumber = fiscalNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "firstName: " + firstName +
+                ", lastName: " + lastName;
+    }
+}
