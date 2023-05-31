ea261da5114315947d744e16fe789be631debafc
Gaston Pioli <milton.pioli@despegar.com>
2023-05-31 20:22:31 -0300

    Abstract classes

---
+++ _009-POO-Abstract/src/org/piolig/pooabstractclassesform/elements/FormElements.java
@@ -0,0 +1,23 @@
+package org.piolig.pooabstractclassesform.elements;
+
+abstract public class FormElements {
+
+    protected String value;
+    protected String name;
+
+    public FormElements() {
+    }
+
+    public FormElements(String name) {
+        this();
+        this.name = name;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    abstract public String drawHtml();
+
+
+}
