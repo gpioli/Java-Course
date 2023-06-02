29474dcd2c29020b0232fdcc14b6a1dbbbc17a3b
Gaston Pioli <milton.pioli@despegar.com>
2023-06-01 21:53:03 -0300

    Abstract classes

---
+++ _009-POO-Abstract/src/org/piolig/pooabstractclassesform/elements/TextareaForm.java
@@ -0,0 +1,42 @@
+package org.piolig.pooabstractclassesform.elements;
+
+public class TextareaForm extends FormElements{
+
+    private int rows;
+    private int columns;
+
+    public TextareaForm(String name) {
+        super(name);
+    }
+
+    public TextareaForm(String name, int rows, int columns) {
+        super(name);
+        this.rows = rows;
+        this.columns = columns;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+
+    @Override
+    public String drawHtml() {
+        return "<textarea name='' cols='' rows=''>" +
+                this.value +
+                this.columns +
+                this.rows +
+                "</textarea>";
+    }
+}
