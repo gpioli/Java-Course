1f3acbded51d29993b2273c17f8c5cfa5c551e9f
Gaston Pioli <milton.pioli@despegar.com>
2023-06-03 00:47:25 -0300

    Abstract classes

---
+++ _009-POO-Abstract/src/org/piolig/pooabstractclasses/form/validator/LengthValidator.java
@@ -0,0 +1,47 @@
+package org.piolig.pooabstractclasses.form.validator;
+
+public class LengthValidator extends Validator{
+
+    // we use a template, in order to be used later with String.format | %d stands for digit, but we could've used
+    // %s and replace it with a string
+    protected String message = "field must have a minimum of %d characters and a maximum of %d characters";
+    private int min;
+    private int max = Integer.MAX_VALUE;
+
+
+    public LengthValidator() {
+    }
+
+    public LengthValidator(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    @Override
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+
+    @Override
+    public boolean isValid(String value) {
+        this.message = String.format(this.message, this.min, this.max);
+        if (value == null){
+            return true;
+        }
+        int length = value.length();
+        return (length >= min && length <= max);
+    }
+}
