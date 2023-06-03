80b783c165871e9b35ff190d6f62515b3f5987fe
Gaston Pioli <milton.pioli@despegar.com>
2023-06-03 00:53:41 -0300

    Abstract classes

---
+++ _009-POO-Abstract/src/org/piolig/pooabstractclasses/form/validator/EmailValidator.java
@@ -0,0 +1,26 @@
+package org.piolig.pooabstractclasses.form.validator;
+
+public class EmailValidator extends Validator{
+
+    // we will use regular expressions in order to validate this... but a simple one.
+    // not the best
+
+    protected String message = "email format is not valid";
+
+    private final static String EMAIL_REGEX = "^(.+)@(.+)$";
+
+    @Override
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+
+    @Override
+    public boolean isValid(String value) {
+        return value.matches(EMAIL_REGEX);
+    }
+}
