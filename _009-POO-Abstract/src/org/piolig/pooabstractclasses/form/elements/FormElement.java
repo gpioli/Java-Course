655d425d390394c4c66d1b6284b86a21ef3410eb
Gaston Pioli <milton.pioli@despegar.com>
2023-06-04 20:06:05 -0300

    Abstract classes

--- _009-POO-Abstract/src/org/piolig/pooabstractclasses/form/elements/FormElement.java
+++ _009-POO-Abstract/src/org/piolig/pooabstractclasses/form/elements/FormElement.java
@@ -1,11 +1,20 @@
 package org.piolig.pooabstractclasses.form.elements;
 
+import org.piolig.pooabstractclasses.form.validator.Validator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 abstract public class FormElement {
 
     protected String value;
     protected String name;
+    private List<Validator> validatorList;
+    private List<String> errors;
 
     public FormElement() {
+        this.validatorList = new ArrayList<>();
+        this.errors = new ArrayList<>();
     }
 
     public FormElement(String name) {
@@ -13,9 +22,25 @@ abstract public class FormElement {
         this.name = name;
     }
 
+    public FormElement addValidator(Validator validator) {
+        this.validatorList.add(validator);
+        return this;
+    }
+
     public void setValue(String value) {
         this.value = value;
     }
+
+    public boolean isValid() {
+        for (Validator v : validatorList) {
+            if (!v.isValid(this.value)) {
+                this.errors.add(v.getMessage());
+            }
+        }
+        return this.errors.size() == 0;
+    }
+
+
 
     abstract public String drawHtml();
 
