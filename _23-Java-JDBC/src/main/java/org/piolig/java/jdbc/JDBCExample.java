958582cb40022c4863e9a9a598e76931ca4515e8
Gaston Pioli <milton.pioli@despegar.com>
2023-10-16 23:18:02 -0300

    JDBC

---
+++ _23-Java-JDBC/src/main/java/org/piolig/java/jdbc/JDBCExample.java
@@ -0,0 +1,33 @@
+package org.piolig.java.jdbc;
+
+import java.sql.*;
+
+public class JDBCExample {
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3307/java_course?serverTimezone=America/Montevideo";
+        String username = "root";
+        String password = "sasa";
+        try (
+                Connection conn = DriverManager.getConnection(url, username, password);
+                Statement stmt = conn.createStatement();
+                ResultSet result = stmt.executeQuery("SELECT * FROM products");
+                ) {
+
+            while (result.next()){
+                System.out.print(result.getInt("id"));
+                System.out.print(" | ");
+                System.out.print(result.getString("name"));
+                System.out.print(" | ");
+                System.out.print(result.getInt("price"));
+                System.out.print(" | ");
+                System.out.println(result.getDate("registry_date"));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+
+        }
+
+    }
+}
