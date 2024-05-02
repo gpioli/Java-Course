14631595e6fbfd5a7f5966e2ccfe7e9191588c00
Gaston Pioli <milton.pioli@despegar.com>
2024-05-02 20:40:26 -0300

    updates

--- _028-Junit5-app/src/test/java/org/piolig/junit5app/examples/models/AccountTest.java
+++ _028-Junit5-app/src/test/java/org/piolig/junit5app/examples/models/AccountTest.java
@@ -2,13 +2,22 @@ package org.piolig.junit5app.examples.models;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.piolig.junit5app.examples.exceptions.InsufficientBalanceException;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 class AccountTest {
 
@@ -36,60 +45,91 @@ class AccountTest {
         System.out.println("Finishing test");
     }
 
-    @Test
-    @DisplayName("Testing account name field")
-    void testAccountName() {
+    @Tag("account")
+    @Nested
+    @DisplayName("Testing account attributes.")
+    class AccountNameAndBalanceTest {
 
-        account.setPerson("Gaston");
+        @Test
+        @DisplayName("Testing name field")
+        void testAccountName() {
 
-        String expected = "Gaston";
-        String real = account.getPerson();
-        assertNotNull(real, "Account can't be null");
+            account.setPerson("Gaston");
 
-        assertEquals(expected, real, "Incorrect name account: expected: " + expected +
-                " actual: " + real);
-        // using lambdas help us save resources, as the string object isn't instantiated unless
-        // the assert condition isn't fulfilled
-        assertTrue(real.equals("Gaston"), () -> "The account name differs from the expected");
-    }
+            String expected = "Gaston";
+            String real = account.getPerson();
+            assertNotNull(real, "Account can't be null");
 
-    @Test
-    @DisplayName("Testing Account balance: not null, greater than cero, expected value")
-    void testAccountBalance() {
-        assertNotNull(account.getBalance());
-        assertEquals(1000.12345, account.getBalance().doubleValue());
-        assertFalse(account.getBalance().compareTo(BigDecimal.ZERO) < 0);
-        assertTrue(account.getBalance().compareTo(BigDecimal.ZERO) > 0);
-    }
+            assertEquals(expected, real, "Incorrect name account: expected: " + expected +
+                    " actual: " + real);
+            // using lambdas help us save resources, as the string object isn't instantiated unless
+            // the assert condition isn't fulfilled
+            assertTrue(real.equals("Gaston"), () -> "The account name differs from the expected");
+        }
 
-    // different objects
-    @Test
-    @DisplayName("Testing account references")
-    void testAccountReference() {
-        account = new Account("John Doe", new BigDecimal(89000.9997));
-        Account account2 = new Account("John Doe", new BigDecimal(89000.9997));
+        @Test
+        @DisplayName("Testing balance: not null, greater than cero, expected value")
+        void testAccountBalance() {
+            assertNotNull(account.getBalance());
+            assertEquals(1000.12345, account.getBalance().doubleValue());
+            assertFalse(account.getBalance().compareTo(BigDecimal.ZERO) < 0);
+            assertTrue(account.getBalance().compareTo(BigDecimal.ZERO) > 0);
+        }
 
-        //assertNotEquals(account2, account);
-        assertEquals(account2, account);
-    }
+        // different objects
+        @Test
+        @DisplayName("Testing account references")
+        void testAccountReference() {
+            account = new Account("John Doe", new BigDecimal(89000.9997));
+            Account account2 = new Account("John Doe", new BigDecimal(89000.9997));
 
-    @Test
-    void testAccountDebit() {
-        account.debit(new BigDecimal(100));
-        assertNotNull(account.getBalance());
-        assertEquals(900, account.getBalance().intValue());
-        assertEquals("900.12345", account.getBalance().toPlainString());
+            //assertNotEquals(account2, account);
+            assertEquals(account2, account);
+        }
 
     }
 
-    @Test
-    void testAccountCredit() {
-        account.credit(new BigDecimal(100));
-        assertNotNull(account.getBalance());
-        assertEquals(1100, account.getBalance().intValue());
-        assertEquals("1100.12345", account.getBalance().toPlainString());
+    @Nested
+    class AccountOperationsTest {
+
+        @Tag("account")
+        @Test
+        void testAccountDebit() {
+            account.debit(new BigDecimal(100));
+            assertNotNull(account.getBalance());
+            assertEquals(900, account.getBalance().intValue());
+            assertEquals("900.12345", account.getBalance().toPlainString());
+
+        }
+
+        @Tag("account")
+        @Test
+        void testAccountCredit() {
+            account.credit(new BigDecimal(100));
+            assertNotNull(account.getBalance());
+            assertEquals(1100, account.getBalance().intValue());
+            assertEquals("1100.12345", account.getBalance().toPlainString());
+        }
+
+        @Tag("account")
+        @Tag("bank")
+        @Test
+        void testTransference() {
+            account = new Account("John Doe", new BigDecimal(2500));
+            Account account2 = new Account("Andres", new BigDecimal("1500.8989"));
+
+            Bank bank = new Bank();
+            bank.setName("State Bank");
+            bank.transference(account2, account, new BigDecimal(500));
+
+            assertEquals("1000.8989", account2.getBalance().toPlainString());
+            assertEquals("3000", account.getBalance().toPlainString());
+        }
+
     }
 
+    @Tag("account")
+    @Tag("error")
     @Test
     void insufficientBalanceExceptionAccount() {
         Exception exception = assertThrows(InsufficientBalanceException.class, () -> {
@@ -102,18 +142,7 @@ class AccountTest {
         assertEquals(expected, actual);
     }
 
-    @Test
-    void testTransference() {
-        account = new Account("John Doe", new BigDecimal(2500));
-        Account account2 = new Account("Andres", new BigDecimal("1500.8989"));
 
-        Bank bank = new Bank();
-        bank.setName("State Bank");
-        bank.transference(account2, account, new BigDecimal(500));
-
-        assertEquals("1000.8989", account2.getBalance().toPlainString());
-        assertEquals("3000", account.getBalance().toPlainString());
-    }
 
     @Test
     @DisplayName("Testing relationships between the bank account and the bank with assertAll")
@@ -144,87 +173,224 @@ class AccountTest {
 
     }
 
-    @Test
-    @EnabledOnOs(OS.WINDOWS)
-    void testOnlyWindows() {
+    @Nested
+    class OperativeSystemTest {
+        @Test
+        @EnabledOnOs(OS.WINDOWS)
+        void testOnlyWindows() {
+        }
+
+        @Test
+        @EnabledOnOs({OS.LINUX, OS.MAC})
+        void testOnlyLinuxMac() {
+        }
+
+        @Test
+        @DisabledOnOs({OS.WINDOWS})
+        void testNoWindows() {
+        }
+
+    }
+
+    @Nested
+    class JavaVersionTest {
+        @Test
+        @EnabledOnJre(JRE.JAVA_8)
+        void testOnlyJdk8() {
+        }
+
+        @Test
+        @EnabledOnJre(JRE.JAVA_11)
+        void testOnlyJDK11() {
+        }
+
+        @Test
+        @DisabledOnJre(JRE.JAVA_15)
+        void testNoJDK15() {
+        }
+
+    }
+
+    @Nested
+    class SystemPropertiesTest {
+
+        // Prints system properties...
+        @Test
+        void systemProperties() {
+            Properties properties = System.getProperties();
+            properties.forEach((k,v) -> System.out.println(k + ": " + v));
+        }
+
+        // Another way of testing according to java version
+        @Test
+        @EnabledIfSystemProperty(named = "java.version", matches = "11")
+        void testJavaVersion() {
+        }
+
+        @Test
+        @DisabledIfSystemProperty(named= "os.arch", matches = ".*32.*")
+        void testOnly64() {
+        }
+
+        @Test
+        @EnabledIfSystemProperty(named= "os.arch", matches = ".*32.*")
+        void testNo64() {
+        }
+
+        @Test
+        @EnabledIfSystemProperty(named= "user.name", matches = "milton")
+        void testUserName() {
+        }
+
+        @Test
+        @EnabledIfSystemProperty(named = "ENV", matches = "dev")
+        void testDev() {
+        }
+
+    }
+
+    class EnvironmentVariablesTest {
+
+        @Test
+        void testPrintEnvironmentVars() {
+            Map<String, String> getEnv = System.getenv();
+            getEnv.forEach((k, v) -> System.out.println(k + ": " + v));
+        }
+
+        @Test
+        @EnabledIfEnvironmentVariable(named = "HOME", matches = "/home/milton")
+        void testHome() {
+        }
+
+        @Test
+        @EnabledIfEnvironmentVariable(named = "PAPERSIZE", matches = "a4")
+        void testProcessors() {
+        }
+
+        @Test
+        @EnabledIfEnvironmentVariable(named = "ENVIRONMENT", matches = "dev")
+        void testEnv() {
+        }
+
     }
 
     @Test
-    @EnabledOnOs({OS.LINUX, OS.MAC})
-    void testOnlyLinuxMac() {
+    @DisplayName("Testing Account balance: not null, greater than cero, expected value" +
+            "in dev environment.")
+    void testAccountBalanceDev() {
+        boolean isDev = "dev".equals(System.getProperty("ENV"));
+
+        //
+        assumeTrue(isDev);
+        assertNotNull(account.getBalance());
+        assertEquals(1000.12345, account.getBalance().doubleValue());
+        assertFalse(account.getBalance().compareTo(BigDecimal.ZERO) < 0);
+        assertTrue(account.getBalance().compareTo(BigDecimal.ZERO) > 0);
     }
 
     @Test
-    @DisabledOnOs({OS.WINDOWS})
-    void testNoWindows() {
+    @DisplayName("Testing Account balance: not null, greater than cero, expected value" +
+            "in dev environment - test 2.")
+    void testAccountBalanceDev2() {
+        boolean isDev = "dev".equals(System.getProperty("ENV"));
+
+        // this code wont run in case environment != dev
+        assumingThat(isDev, ()-> {
+            assertNotNull(account.getBalance());
+            assertEquals(1000.12345, account.getBalance().doubleValue());
+        });
+        assertFalse(account.getBalance().compareTo(BigDecimal.ZERO) < 0);
+        assertTrue(account.getBalance().compareTo(BigDecimal.ZERO) > 0);
+
     }
 
-    @Test
-    @EnabledOnJre(JRE.JAVA_8)
-    void testOnlyJdk8() {
+    @DisplayName("Testing account debit with repetitions")
+    @RepeatedTest(value = 5, name = "{displayName} - Repetition number: {currentRepetition} of {totalRepetitions}")
+    void testAccountDebitRepeated(RepetitionInfo repetitionInfo) {
+        if (repetitionInfo.getCurrentRepetition() == 3) {
+            System.out.println("We are in the third repetition!");
+        }
+        account.debit(new BigDecimal(100));
+        assertNotNull(account.getBalance());
+        assertEquals(900, account.getBalance().intValue());
+        assertEquals("900.12345", account.getBalance().toPlainString());
+
     }
 
-    @Test
-    @EnabledOnJre(JRE.JAVA_11)
-    void testOnlyJDK11() {
+
+    @Tag("param")
+    @Nested
+    class ParametrizedTests {
+        @ParameterizedTest(name = "Repetition number {index}. Executing with value: {0} - {argumentsWithNames}")
+        @ValueSource(strings = {"100", "200", "300", "500", "700", "1000"})
+        void testAccountValueSource(String amount) {
+
+            account.debit(new BigDecimal(amount));
+            assertNotNull(account.getBalance());
+            assertTrue(account.getBalance().compareTo(BigDecimal.ZERO) >= 0);
+        }
+
+        @ParameterizedTest(name = "Repetition number {index}. Executing with value: {0} - {argumentsWithNames}")
+        @CsvSource({"1,100", "2,200", "3,300", "4,500", "5,700", "6,1000"})
+        void testAccountDebitCsvSource(String index, String amount) {
+            System.out.println(index + " -> " + amount);
+            account.debit(new BigDecimal(amount));
+            assertNotNull(account.getBalance());
+            assertTrue(account.getBalance().compareTo(BigDecimal.ZERO) >= 0);
+        }
+
+        @ParameterizedTest(name = "Repetition number {index}. Executing with value: {0} - {argumentsWithNames}")
+        @CsvFileSource(resources = "/data.csv")
+        void testAccountDebitCsvFileSource(String amount) {
+            account.debit(new BigDecimal(amount));
+            assertNotNull(account.getBalance());
+            assertTrue(account.getBalance().compareTo(BigDecimal.ZERO) >= 0);
+        }
+
+        @ParameterizedTest(name = "Repetition number {index}. Executing with value: {0} - {argumentsWithNames}")
+        @CsvSource({"200,100, John, Andres", "250,200, Pepe, Pepe", "300,300, Maria, maria", "510,500, Pepa, Pepe", "750,700, Lucas, Luca", "1000.12345,1000.12345, Cata, Cata"})
+        void testAccountDebitCsvSource2(String balance, String amount, String expected, String actual) {
+            System.out.println(balance + " -> " + amount);
+            account.setBalance(new BigDecimal(balance));
+            account.debit(new BigDecimal(amount));
+            account.setPerson(actual);
+
+
+            assertNotNull(account.getBalance());
+            assertNotNull(account.getPerson());
+            assertEquals(expected, actual);
+            assertTrue(account.getBalance().compareTo(BigDecimal.ZERO) >= 0);
+        }
+
+        @ParameterizedTest(name = "Repetition number {index}. Executing with value: {0} - {argumentsWithNames}")
+        @CsvFileSource(resources = "/data2.csv")
+        void testAccountDebitCsvFileSource2(String balance, String amount, String expected, String actual) {
+            System.out.println(balance + " -> " + amount);
+            account.setBalance(new BigDecimal(balance));
+            account.debit(new BigDecimal(amount));
+            account.setPerson(actual);
+
+
+            assertNotNull(account.getBalance());
+            assertNotNull(account.getPerson());
+            assertEquals(expected, actual);
+            assertTrue(account.getBalance().compareTo(BigDecimal.ZERO) >= 0);
+        }
     }
 
-    @Test
-    @DisabledOnJre(JRE.JAVA_15)
-    void testNoJDK15() {
+    @Tag("param")
+    @ParameterizedTest(name = "Repetition number {index}. Executing with value: {0} - {argumentsWithNames}")
+    @MethodSource("amountList")
+    void testAccountDebitMethodSource(String amount) {
+        account.debit(new BigDecimal(amount));
+        assertNotNull(account.getBalance());
+        assertTrue(account.getBalance().compareTo(BigDecimal.ZERO) >= 0);
     }
 
-    // Prints system properties...
-    @Test
-    void systemProperties() {
-        Properties properties = System.getProperties();
-        properties.forEach((k,v) -> System.out.println(k + ": " + v));
+    static List<String> amountList(){
+        return Arrays.asList("100", "200", "300", "500", "700", "1000");
     }
 
-    // Another way of testing according to java version
-    @Test
-    @EnabledIfSystemProperty(named = "java.version", matches = "11")
-    void testJavaVersion() {
-    }
 
-    @Test
-    @DisabledIfSystemProperty(named= "os.arch", matches = ".*32.*")
-    void testOnly64() {
-    }
 
-    @Test
-    @EnabledIfSystemProperty(named= "os.arch", matches = ".*32.*")
-    void testNo64() {
-    }
-
-    @Test
-    @EnabledIfSystemProperty(named= "user.name", matches = "milton")
-    void testUserName() {
-    }
-
-    @Test
-    @EnabledIfSystemProperty(named = "ENV", matches = "dev")
-    void testDev() {
-    }
-
-    @Test
-    void testPrintEnvironmentVars() {
-        Map<String, String> getEnv = System.getenv();
-        getEnv.forEach((k, v) -> System.out.println(k + ": " + v));
-    }
-
-    @Test
-    @EnabledIfEnvironmentVariable(named = "HOME", matches = "/home/milton")
-    void testHome() {
-    }
-
-    @Test
-    @EnabledIfEnvironmentVariable(named = "PAPERSIZE", matches = "a4")
-    void testProcessors() {
-    }
-
-    @Test
-    @EnabledIfEnvironmentVariable(named = "ENVIRONMENT", matches = "dev")
-    void testEnv() {
-    }
 }
