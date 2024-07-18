aa6cdd62fbde25e4b50cba8505d9caf9888d668d
Gaston Pioli <milton.pioli@despegar.com>
2024-07-18 01:04:45 -0300

    Cookies

--- _33-webapp-cookies/src/main/java/org/piolig/apiservlet/webapp/headers/contollers/LoginServlet.java
+++ _33-webapp-cookies/src/main/java/org/piolig/apiservlet/webapp/headers/contollers/LoginServlet.java
@@ -2,18 +2,51 @@ package org.piolig.apiservlet.webapp.headers.contollers;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.piolig.apiservlet.webapp.headers.service.LoginService;
+import org.piolig.apiservlet.webapp.headers.service.LoginServiceImpl;
 
+import javax.swing.text.html.Option;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Optional;
 
-@WebServlet("/login")
+@WebServlet({"/login", "/login.html"})
 public class LoginServlet extends HttpServlet {
 
     final static String USERNAME = "admin";
     final static String PASSWORD = "12345";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LoginService auth = new LoginServiceImpl();
+        Optional<String> cookieOptional = auth.getUsername(req);
+
+        if (cookieOptional.isPresent()) {
+            resp.setContentType("text/html;charset=UTF-8");
+            try (PrintWriter out = resp.getWriter()) {
+
+                out.println("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("    <head>");
+                out.println("        <meta charset=\"UTF-8\">");
+                out.println("        <title> Hello " + cookieOptional.get() + "</title>");
+                out.println("    </head>");
+                out.println("    <body>");
+                out.println("        <h1>Hello " + cookieOptional.get() + " you've successfully logged in! " + "</h1>");
+                out.println("        <p><a href='" + req.getContextPath() + "/index.html'>Return</a></p>");
+                out.println("        <p><a href='" + req.getContextPath() + "/logout'>Logout</a></p>");
+                out.println("    </body>");
+                out.println("</html>");
+            }
+        } else {
+            getServletContext().getRequestDispatcher("/login.jsp").forward(req, resp);
+        }
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -23,21 +56,10 @@ public class LoginServlet extends HttpServlet {
 
         if (USERNAME.equals(username) && PASSWORD.equals(password)) {
 
-            resp.setContentType("text/html;charset=UTF-8");
-            try (PrintWriter out = resp.getWriter()) {
+            Cookie usernameCookie = new Cookie("username", username);
+            resp.addCookie(usernameCookie);
 
-                out.println("<!DOCTYPE html>");
-                out.println("<html>");
-                out.println("    <head>");
-                out.println("        <meta charset=\"UTF-8\">");
-                out.println("        <title>  Successful login! </title>");
-                out.println("    </head>");
-                out.println("    <body>");
-                out.println("        <h1> Successful login! </h1>");
-                out.println("        <h3> Hello " + username + ". You hace successfully logged in! </h3>");
-                out.println("    </body>");
-                out.println("</html>");
-            }
+            resp.sendRedirect(req.getContextPath() + "/login.html");
 
         } else {
             resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Sorry, not authorized.");
--- _33-webapp-cookies/src/main/java/org/piolig/apiservlet/webapp/headers/contollers/ProductServlet.java
+++ _33-webapp-cookies/src/main/java/org/piolig/apiservlet/webapp/headers/contollers/ProductServlet.java
@@ -2,16 +2,21 @@ package org.piolig.apiservlet.webapp.headers.contollers;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.piolig.apiservlet.webapp.headers.models.Product;
+import org.piolig.apiservlet.webapp.headers.service.LoginService;
+import org.piolig.apiservlet.webapp.headers.service.LoginServiceImpl;
 import org.piolig.apiservlet.webapp.headers.service.ProducService;
 import org.piolig.apiservlet.webapp.headers.service.ProductServiceImpl;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @WebServlet({"/products.html", "/products"})
 public class ProductServlet extends HttpServlet {
@@ -20,6 +25,9 @@ public class ProductServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ProducService service = new ProductServiceImpl();
         List<Product> products = service.list();
+
+        LoginService auth = new LoginServiceImpl();
+        Optional<String> cookieOptional = auth.getUsername(req);
 
         resp.setContentType("text/html;charset=UTF-8");
 
@@ -34,20 +42,28 @@ public class ProductServlet extends HttpServlet {
             out.println("    <body>");
             out.println("    <body>");
             out.println("        <h1>Products list! </h1>");
+            if (cookieOptional.isPresent()) {
+                out.println("<div style='color:blue'>Hello " + cookieOptional.get() + ". Welcome!" + "</div>");
+            }
 
             out.println("        <table>");
             out.println("        <tr>");
             out.println("        <th>id</th>");
             out.println("        <th>name</th>");
             out.println("        <th>type</th>");
-            out.println("        <th>price</th>");
+            if (cookieOptional.isPresent()) {
+                out.println("        <th>price</th>");
+            }
             out.println("        </tr>");
             products.forEach(p -> {
                 out.println("<tr>");
                 out.println("<td>" + p.getId() + "</td>");
                 out.println("<td>" + p.getName() + "</td>");
                 out.println("<td>" + p.getType() + "</td>");
-                out.println("<td>" + p.getPrice() + "</td>");
+                if (cookieOptional.isPresent()) {
+                    out.println("<td>" + p.getPrice() + "</td>");
+                }
+
                 out.println("</tr>");
             });
             out.println("        </table>");
